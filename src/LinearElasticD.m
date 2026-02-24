80fcc1c65508336c12fbfafabe1431975a1c1cf2
Tony_Nguyen <i110142109@nkust.edu.tw>
2026-02-23 20:14:33 -0500

    modify the code in src/LinearElasticD.m file

--- src/LinearElasticD.m
+++ src/LinearElasticD.m
@@ -7,3 +7,22 @@ D0=(E*(1-u))/((1+u)*(1-2*u))*[1 u/(1-u) u/(1-u) 0 0 0
                              0 0 0 0 (1-2*u)/(2*(1-u)) 0
                              0 0 0 0 0 (1-2*u)/(2*(1-u))];
 end
+
+%
+function D = LinearElasticD_PlaneStress(E, nu)
+    % Plane stress D matrix (Voigt: εxx, εyy, γxy)
+    factor = E / (1 - nu^2);
+    D = factor * [1    nu   0;
+                  nu   1    0;
+                  0    0  (1-nu)/2 ];
+end
+
+
+function D = LinearElasticD_PlaneStrain(E, nu)
+    % Plane strain D matrix (Voigt: εxx, εyy, γxy)
+    % This is the effective in-plane stiffness when εzz=0
+    factor = E * (1 - nu) / ((1 + nu) * (1 - 2*nu));
+    D = factor * [1          nu/(1-nu)    0;
+                  nu/(1-nu)  1            0;
+                  0          0          (1-2*nu)/(2*(1-nu)) ];
+end
